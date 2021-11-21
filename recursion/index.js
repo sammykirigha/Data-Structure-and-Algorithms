8cacf638d14d7e997f2606c0ce324482db5c1fe0
Samido <dkirigha18@gmail.com>
2021-11-21 23:27:09 +0300

    added more recursion functions

--- recursion/index.js
+++ recursion/index.js
@@ -1,5 +1,4 @@
 //a for loop function
-
 function getOddnums(arr) {
   let results = [];
   for (let i = 0; i <= arr.length - 1; i++) {
@@ -9,7 +8,6 @@ function getOddnums(arr) {
   }
   return results;
 }
-
 //using a recursion helper function to get odd nums in a array
 function collectOddValues(arr) {
   let results = [];
@@ -23,4 +21,38 @@ function collectOddValues(arr) {
   }
   helper(arr);
   return results;
+}
+
+function countDown(num) {
+  if (num == 0) {
+    console.log("Done");
+    return;
+  }
+  return countDown(num - 1);
+}
+// Here is an array containing both numbers as well as other arrays, which
+// in turn contain numbers and arrays:
+// Write a recursive function that prints all the numbers (and just numbers).
+
+function printNums(arr) {
+  for (value in arr) {
+    if (arr instanceof Array) {
+      printNums(arr);
+    } else {
+      console.log(value);
+    }
+  }
+}
+
+//printing the fibonacci with a time complexity of o(n2)
+function getNthFibo(num) {
+  if (num <= 1) return num;
+  return getNthFibo(num - 1) + getNthFibo(n - 2);
+}
+
+//printing the fibonacci with a time complexity of o(n)
+function getNthFiboBetter(num, lastNum, nextNum) {
+  if (num == 0) return lastNum;
+  if (num == 1) return nextNum;
+  return getNthFiboBetter(num - 1, nextNum, lastNum + nextNum);
 }
